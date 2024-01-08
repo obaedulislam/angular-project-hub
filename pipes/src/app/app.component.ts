d6cbe7c6dbda4a498b95763a2c84761c8396093c
obaed <obaedul@redlimesolutions.com>
2024-01-08 23:46:35 +0600

    Mile to kilometers implemented & custom pipe created

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -12,21 +12,27 @@ export class AppComponent {
   public paymentDate: string | undefined;
   public amount: number | undefined;
   public height: number | undefined;
+  public miles: number | undefined;
+
+  car = {
+    make: "Toyota",
+    model: "Camry",
+    year: 2010,
+  }
 
   onNameChange(event: Event) {
     this.payName = (event.target as HTMLInputElement).value;
-    console.log(this.payName);
   }
   onDateChange(event: Event) {
     this.paymentDate = (event.target as HTMLInputElement).value;
-    console.log(this.paymentDate);
   }
   onAmountChange(event: Event) {
     this.amount = parseFloat((event.target as HTMLInputElement).value);
-    console.log(this.paymentDate);
   }
   onHeightChange(event: Event) {
     this.height = parseFloat((event.target as HTMLInputElement).value);
-    console.log(this.paymentDate);
+  }
+  onMilesChange(event: Event) {
+    this.miles = parseFloat((event.target as HTMLInputElement).value);
   }
 }
