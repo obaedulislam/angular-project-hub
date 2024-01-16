05feffb2661f193e659cc1983339a801a5a18143
obaed <obaedul@redlimesolutions.com>
2024-01-16 12:06:49 +0600

    angular routerlink for menubar added

--- variety-component/src/app/elements/elements-routing.module.ts
+++ variety-component/src/app/elements/elements-routing.module.ts
@@ -4,7 +4,6 @@ import { ElementsHomeComponent } from './elements-home/elements-home.component';
 
 const routes: Routes = [
   { path: "elements", component: ElementsHomeComponent }
-
 ];
 
 @NgModule({
