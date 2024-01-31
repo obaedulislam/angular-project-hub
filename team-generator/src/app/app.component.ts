4751147a88960fffcd72f7fcc6b89111785e94c1
obaed <obaedul@redlimesolutions.com>
2024-02-01 00:26:39 +0600

    Member add & error handling done

--- team-generator/src/app/app.component.ts
+++ team-generator/src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'team-generator';
+  newMemberName = '';
+  members: string[] = [];
+  errorMessage = '';
+
+  onInput(member: string) {
+    this.newMemberName = member;
+    console.log(this.newMemberName);
+  }
+
+  addMember() {
+    if (!this.newMemberName) {
+      this.errorMessage = "Name can't be empty!";
+      return;
+    }
+    this.members.push(this.newMemberName);
+    this.newMemberName = '';
+    this.errorMessage = '';
+  }
+
 }
